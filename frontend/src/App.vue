8af3e0deb1d3fbe728d51a2a7796086884ecb37b
Anthon Holmqvist <anthon.kendel@gmail.com>
2017-11-18 15:10:22 +0100

    init vue project

---
+++ frontend/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+    <div>
+      <v-btn flat>Normal</v-btn>
+      <v-btn flat dark color="primary">Primary</v-btn>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app',
+  };
+</script>
+
+<style>
+</style>
---
+++ frontend/src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+import App from './App';
+import router from './router';
+import store from './shared/store';
+
+Vue.use(Vuetify);
+
+Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  template: '<App/>',
+  components: { App },
+});
