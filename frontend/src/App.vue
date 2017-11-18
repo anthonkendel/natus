c2ff88242138074549a2bb3e5eec798c5feb1805
Anthon Holmqvist <anthon.kendel@gmail.com>
2017-11-18 19:02:58 +0100

    update navigation drawer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,7 +2,9 @@
   <v-app dark id="app">
     <n-navigation></n-navigation>
     <v-content>
-      <v-container fluid><router-view/></v-container>
+      <v-container fluid>
+        <router-view/>
+      </v-container>
     </v-content>
   </v-app>
 </template>
--- frontend/src/components/NNavigation/NNavigation.vue
+++ frontend/src/components/NNavigation/NNavigation.vue
@@ -3,22 +3,25 @@
     app
     :dark="true"
     fixed
-    class="teal lighten-1"
-    :mini-variant="miniVariant"
+    class="deep-orange lighten-2"
+    :mini-variant="mini"
+    v-model="drawer"
     value="true">
 
-    <v-toolbar flat class="teal lighten-1">
+    <v-toolbar flat class="deep-orange lighten-2">
       <v-list>
         <v-list-tile>
-          <v-list-tile-title class="title">
-            Natus
-          </v-list-tile-title>
+          <v-list-tile-title class="title">Natus</v-list-tile-title>
         </v-list-tile>
       </v-list>
     </v-toolbar>
     <v-divider></v-divider>
     <v-list>
-      <v-list-tile v-for="path in paths" :key="path.title" @click="routeTo(path.name)">
+      <v-list-tile
+        v-for="path in paths"
+        :key="path.title"
+        @click="routeTo(path.name)">
+
         <v-list-tile-action>
           <v-icon>{{ path.icon }}</v-icon>
         </v-list-tile-action>
@@ -28,23 +31,21 @@
           </v-list-tile-title>
         </v-list-tile-content>
       </v-list-tile>
+      <v-list-tile class="red lighten-2" @click="toggleMini()">
+        <template v-if="!mini">
+          <v-list-tile-action>
+            <v-icon>keyboard_arrow_left</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-title>Collapse</v-list-tile-title>
+        </template>
+        <template v-if="mini">
+          <v-list-tile-action>
+            <v-icon>keyboard_arrow_right</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-title>Expand</v-list-tile-title>
+        </template>
+      </v-list-tile>
     </v-list>
-    <v-btn
-      small
-      block
-      flat
-      v-show="!miniVariant"
-      @click="toggleMiniVariant">
-      <v-icon>keyboard_arrow_left</v-icon>
-    </v-btn>
-    <v-btn
-      small
-      block
-      flat
-      v-show="miniVariant"
-      @click="toggleMiniVariant">
-      <v-icon>keyboard_arrow_right</v-icon>
-    </v-btn>
   </v-navigation-drawer>
 </template>
 
@@ -53,7 +54,8 @@
     name: 'NNavigation',
     data() {
       return {
-        miniVariant: false,
+        drawer: true,
+        mini: true,
         paths: [
           { title: 'Events', name: 'n-event-list', icon: 'list' },
           { title: 'Create', name: 'n-event-create', icon: 'add' },
@@ -62,10 +64,11 @@
     },
     methods: {
       routeTo(name) {
-        this.$router.push({name});
+        this.$router.push({ name });
       },
-      toggleMiniVariant() {
-        this.miniVariant = !this.miniVariant;
+      toggleMini() {
+        this.mini = !this.mini;
+        this.$nextTick();
       },
     },
   };
