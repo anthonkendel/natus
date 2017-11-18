f1149bd073d7bedc0dde9e5b6e064bdc6e5401d0
Anthon Holmqvist <anthon.kendel@gmail.com>
2017-11-18 19:33:51 +0100

    Update event model and added faker for mocking data

--- frontend/src/components/NEventCreate/NEventCreate.vue
+++ frontend/src/components/NEventCreate/NEventCreate.vue
@@ -1,12 +1,31 @@
 <template>
-  <div>
-    Create
-  </div>
+  <v-form v-model="valid">
+    <v-text-field
+      label="Name"
+      v-model="name"
+      :rules="nameRules"
+      :counter="10"
+      required
+    ></v-text-field>
+    <v-text-field
+      label="E-mail"
+      v-model="email"
+      :rules="emailRules"
+      required
+    ></v-text-field>
+  </v-form>
 </template>
 
 <script>
+  import Event from '../../shared/models/Event';
+
   export default {
     name: 'NEventCreate',
+    data() {
+      return {
+        event: new Event(),
+      };
+    },
   };
 </script>
 
--- frontend/src/shared/models/Event.js
+++ frontend/src/shared/models/Event.js
@@ -1,21 +1,12 @@
 export default class Event {
-  constructor(
-    id,
-    name,
-    desc,
-    imageURL,
-    startDate,
-    endDate,
-    category,
-    location,
-  ) {
-    this.id = id;
-    this.name = name;
-    this.desc = desc;
-    this.imageURL = imageURL;
-    this.startDate = startDate;
-    this.endDate = endDate;
-    this.category = category;
-    this.location = location;
+  constructor({ id, name, description, imageUrl, startDate, endDate, category, location } = {}) {
+    this.id = id || '';
+    this.name = name || '';
+    this.description = description || '';
+    this.imageUrl = imageUrl || '';
+    this.startDate = startDate || '';
+    this.endDate = endDate || '';
+    this.category = category || '';
+    this.location = location || '';
   }
 }
