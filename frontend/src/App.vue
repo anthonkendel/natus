cd23df6a4847553aecb9ad6e7dfd67bb28cf8a67
Anthon Holmqvist <anthon.kendel@gmail.com>
2017-11-18 16:47:00 +0100

    add basic navigation drawer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,20 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+  <v-app dark id="app">
+    <n-navigation></n-navigation>
+    <v-content>
+      <v-container fluid><router-view/></v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
+  import NNavigation from './components/NNavigation/NNavigation';
+
   export default {
     name: 'app',
+    components: {
+      NNavigation,
+    },
   };
 </script>
 
---
+++ frontend/src/components/NNavigation/NNavigation.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-navigation-drawer
+    app
+    :dark="true"
+    fixed
+    class="teal lighten-1"
+    :mini-variant="miniVariant"
+    value="true">
+
+    <v-list>
+      <v-list-tile v-for="path in paths" :key="path.title" @click="routeTo(path.name)">
+        <v-list-tile-action>
+          <v-icon>{{ path.icon }}</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-content>
+          <v-list-tile-title>
+              {{ path.title }}
+          </v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+  export default {
+    name: 'NNavigation',
+    data() {
+      return {
+        miniVariant: false,
+        paths: [
+          { title: 'Events', name: 'n-event-list', icon: 'dashboard' },
+          { title: 'Create', name: 'n-event-create', icon: 'question_answer' },
+        ],
+      };
+    },
+    methods: {
+      routeTo(name) {
+        this.$router.push({name});
+      },
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+</style>
