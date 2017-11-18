95f9aee6df6894720e4c00810f73febcd0c356fb
Anthon Holmqvist <anthon.kendel@gmail.com>
2017-11-18 17:21:17 +0100

    add small navigation toggle

--- frontend/src/components/NNavigation/NNavigation.vue
+++ frontend/src/components/NNavigation/NNavigation.vue
@@ -7,6 +7,16 @@
     :mini-variant="miniVariant"
     value="true">
 
+    <v-toolbar flat class="teal lighten-1">
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-title class="title">
+            Natus
+          </v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-toolbar>
+    <v-divider></v-divider>
     <v-list>
       <v-list-tile v-for="path in paths" :key="path.title" @click="routeTo(path.name)">
         <v-list-tile-action>
@@ -19,6 +29,22 @@
         </v-list-tile-content>
       </v-list-tile>
     </v-list>
+    <v-btn
+      small
+      block
+      flat
+      v-show="!miniVariant"
+      @click="toggleMiniVariant">
+      <v-icon>keyboard_arrow_left</v-icon>
+    </v-btn>
+    <v-btn
+      small
+      block
+      flat
+      v-show="miniVariant"
+      @click="toggleMiniVariant">
+      <v-icon>keyboard_arrow_right</v-icon>
+    </v-btn>
   </v-navigation-drawer>
 </template>
 
@@ -29,14 +55,17 @@
       return {
         miniVariant: false,
         paths: [
-          { title: 'Events', name: 'n-event-list', icon: 'dashboard' },
-          { title: 'Create', name: 'n-event-create', icon: 'question_answer' },
+          { title: 'Events', name: 'n-event-list', icon: 'list' },
+          { title: 'Create', name: 'n-event-create', icon: 'add' },
         ],
       };
     },
     methods: {
       routeTo(name) {
         this.$router.push({name});
+      },
+      toggleMiniVariant() {
+        this.miniVariant = !this.miniVariant;
       },
     },
   };
